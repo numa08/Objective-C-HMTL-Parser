6013998401237bceade55671264a52f3556d47e2
numa08 <n511287@gmail.com>
2014-12-11 23:57:41 +0900

    Initial commit

---
+++ Example/NNHTMLParser/CPDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NNAppDelegate.h
+//  NNHTMLParser
+//
+//  Created by CocoaPods on 12/11/2014.
+//  Copyright (c) 2014 numa08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
